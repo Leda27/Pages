964d8320164e4796be9dd364d5436b86fee5064c
srcardo21 <srcardo21@gmial.com>
2024-10-12 20:04:49 -0500

    Se añade el botón de volver al inicio

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let despues = document.querySelector('.despues')
 let antes = document.querySelector('.antes')
+let volver = document.querySelector('.Volver')
 
 despues.addEventListener('click', function(){
     let objetos = document.querySelectorAll('.objeto')
@@ -9,4 +10,8 @@ despues.addEventListener('click', function(){
 antes.addEventListener('click', function(){
     let objetos = document.querySelectorAll('.objeto')
     document.querySelector('.slide').prepend(objetos[objetos.length - 1])
+})  
+
+volver.addEventListener('click', function(){
+    window.location.href = '../index.html'
 })  
